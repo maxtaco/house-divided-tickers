e8ecb42d4644b38ae6c612a393135ea7ebd66fba
Maxwell Krohn <themax@gmail.com>
2012-07-30 23:35:52 -0400

    not working since i think yahoo is stopping me based on user/agent or referrer.  need to work more on this tomorrow

--- pull_date.py
+++ pull_date.py
@@ -2,17 +2,22 @@
 
 import urllib.request
 import datetime
+import re
+
+class MyError (Exception):
+    pass
 
 class QuoteGetter:
     def __init__ (self, date, ticker):
         self._date = date
         self._ticker = ticker
+        self._re = re.compile(r'class="yfnc_tabledata1"', re.M)
 
     def fetch (self):
         date2 = self._date
         date1 = self._date + datetime.timedelta(20)
-        prfx = "http://finance.yahoo.com/q/hp?"
-        fmt = "{prfx}s={ticker}&a={m1}&b={d1}&c={y1}&d={m2}&e={d2}&f={y2}&g=d"
+        prfx = "http://ichart.finance.yahoo.com/table.csv?"
+        fmt = "{prfx}s={ticker}&a={m1}&b={d1}&c={y1}&d={m2}&e={d2}&f={y2}&g=d&ignore=.csv"
         url = fmt.format( prfx = prfx,
                           ticker = self._ticker,
                           m1 = date1.month - 1,
@@ -26,8 +31,13 @@ class QuoteGetter:
         self._body = self._res.read()
         return self._body
 
+    def parse (self):
+        m = self._re.search ( str(self._body ))
+        if not m:
+            raise MyError ("cannot find matching body for ticker={0}".format (self._ticker))
 
 gq = QuoteGetter(date = datetime.datetime(2009,9,20), ticker = "GS")
 print(gq.fetch())
+gq.parse()
 
         
